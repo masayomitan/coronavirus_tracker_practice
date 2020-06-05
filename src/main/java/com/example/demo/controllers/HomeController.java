68e0079503aacbd07ef5883d5c5e7c07366d29ea
kawasaki <yomitanmasa3523@gmail.com>
2020-06-05 09:23:05 +0900

    Update HomeController.java

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controllers;
 
+import com.example.demo.services.CoronaVirusDataGlobal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,10 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 
+  //@AutowiredでCoronavirusDataGlobalのフィールド単位で付与する。
+  //付与されたフィールドの型と合うBeanを自動的にDIしてくれる。型と合うBeanが複数ある場合は、@Qualifierアノテーションを使用して一意に識別させる。
+  @Autowired
+  CoronaVirusDataGlobal coronaVirusDataGlobal;
 
   @GetMapping("/")
   public String home(){
     return "home";
   }
   
+
 }
