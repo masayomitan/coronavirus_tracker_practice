2099d6894ca27fef908906b6dfa968c66f4b8185
kawasaki <yomitanmasa3523@gmail.com>
2020-06-09 11:25:27 +0900

    deathstats

--- src/main/java/com/example/demo/services/coronaVirusDataDeath.java
+++ src/main/java/com/example/demo/services/coronaVirusDataDeath.java
@@ -7,10 +7,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import com.example.demo.models.LocationStats;
+import com.example.demo.models.DeathStats;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -20,11 +21,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class coronaVirusDataDeath {
 
-  private static String VIRUS_DEATH_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+  private static String VIRUS_DEATH_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
   
   private List<DeathStats> allDeathStats = new ArrayList<>();
 
-  public List<DeathStsts> getAllDeathStats() {
+  public List<DeathStats> getAllDeathStats() {
     return allDeathStats;
   }
 
@@ -32,7 +33,7 @@ public class coronaVirusDataDeath {
   @Scheduled
   public void fetchVirusData() throws IOException, InterruptedException{
 
-    List<DeathStats> newDeathSTats = new ArrayList<>();
+    List<DeathStats> newDeathStats = new ArrayList<>();
     HttpClient client = HttpClient.newHttpClient();
       HttpRequest request = HttpRequest.newBuilder()
         .uri(URI.create(VIRUS_DEATH_DATA_URL))
@@ -42,16 +43,16 @@ public class coronaVirusDataDeath {
     Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
 
     for(CSVRecord record : records){
-      LocationStats locationStat = new LocationStats();
-      locationStat.setState(record.get("Province/State"));
-      locationStat.setCountry(record.get("Country/Region"));
+      DeathStats DeathStat = new DeathStats();
+      DeathStat.setState(record.get("Province/State"));
+      DeathStat.setCountry(record.get("Country/Region"));
 
       int latestCases = Integer.parseInt(record.get(record.size() - 1));
       //-2は前日までの情報
       int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
-      locationStat.setLatestTotalCases(latestCases);
-      locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
-      newDeathStats.add(locationStat);
+      DeathStat.setLatestTotalCases(latestCases);
+      DeathStat.setDiffFromPrevDay(latestCases - prevDayCases);
+      newDeathStats.add(DeathStat);
     }
     this.allDeathStats = newDeathStats;
     }    
